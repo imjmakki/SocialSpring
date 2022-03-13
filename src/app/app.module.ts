e59d32c9dccf9bd49c99cd968b22ff764afd24b9
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-13 17:14:14 +0300

    adding layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import {Routes} from "@angular/router";
+import { SocialComponent } from './layout/social/social.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ProfileComponent } from './layout/profile/profile.component';
 
 const routes: Routes = [
-
+  {path: '/login', component: SocialComponent},
+  {path: '/home', component: HomeComponent},
+  {path: '/profile', component: ProfileComponent},
+  {path: '**', component: },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SocialComponent,
+    HomeComponent,
+    HomeprofileComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
