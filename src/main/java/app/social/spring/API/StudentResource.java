76caeab5232acffb0fed1808215e13633d7dbab2
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 17:02:53 +0300

    adding api

--- src/main/java/app/social/spring/API/StudentResource.java
+++ src/main/java/app/social/spring/API/StudentResource.java
@@ -1,9 +1,27 @@
 package app.social.spring.API;
 
+import app.social.spring.Entity.Student;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
 public class StudentResource {
+
+    private List<Student> students = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        students.add(new Student(1L, "Mohamad J Makki", "120"));
+        students.add(new Student(2L, "Nabeel D Hussein", "20"));
+        students.add(new Student(3L, "Hassan A Hummadi", "23"));
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
 }
