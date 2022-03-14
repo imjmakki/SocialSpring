88df3ef3635a4f472b6ecc8ca0ab82d371712bdf
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 17:24:03 +0300

    adding security configuration

---
+++ src/main/java/app/social/spring/Entity/User.java
@@ -0,0 +1,23 @@
+package app.social.spring.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String email;
+    private String password;
+}
