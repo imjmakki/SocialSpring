c3218bcff25688990a88c6b03910513a0c4dc61d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 15:26:25 +0300

    starting the back end for social login

--- src/main/java/app/social/spring/API/GoogleResource.java
+++ src/main/java/app/social/spring/API/GoogleResource.java
@@ -1,6 +1,10 @@
 package app.social.spring.API;
 
+import app.social.spring.DTO.TokenDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class GoogleResource {
+
+    @Value("${google.id}")
+    private String IdClient;
+
+    public ResponseEntity<?> loginWithGoogle(@RequestBody TokenDTO tokenDTO) {
+
+    }
 }
---
+++ src/main/java/app/social/spring/DTO/TokenDTO.java
@@ -0,0 +1,14 @@
+package app.social.spring.DTO;
+
+public class TokenDTO {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
