0831d408918058df14ab9fe4cad04f583ca6cbae
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 16:51:06 +0300

    Entities Adding

---
+++ src/main/java/app/social/spring/Entity/Student.java
@@ -0,0 +1,10 @@
+package app.social.spring.Entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    private Long id;
+    private String name;
+    private String age;
+}
