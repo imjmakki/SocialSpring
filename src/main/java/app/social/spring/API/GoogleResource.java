0de8599cc2bd1dbd8891f03e4cfd5f43e6f35d46
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 15:47:20 +0300

    Google Connection

--- src/main/java/app/social/spring/API/GoogleResource.java
+++ src/main/java/app/social/spring/API/GoogleResource.java
@@ -1,6 +1,7 @@
 package app.social.spring.API;
 
 import app.social.spring.DTO.TokenDTO;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Collections;
 
 @RestController
@@ -21,11 +23,12 @@ public class GoogleResource {
     @Value("${google.id}")
     private String IdClient;
 
-    public ResponseEntity<?> loginWithGoogle(@RequestBody TokenDTO tokenDTO) {
+    public ResponseEntity<?> loginWithGoogle(@RequestBody TokenDTO tokenDTO) throws IOException {
         NetHttpTransport transport = new NetHttpTransport();
         JacksonFactory factory = new JacksonFactory();
         GoogleIdTokenVerifier.Builder ver =
                 new GoogleIdTokenVerifier.Builder(transport, factory)
                         .setAudience(Collections.singleton(IdClient));
+        GoogleIdToken googleIdToken = GoogleIdToken.parse(ver.getJsonFactory(), tokenDTO.getToken());
     }
 }
