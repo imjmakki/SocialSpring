b92adc672dbd8c1378fd06a3f85d5a93f56bc1e0
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 15:40:26 +0300

    starting the back end for social login

--- src/main/java/app/social/spring/API/GoogleResource.java
+++ src/main/java/app/social/spring/API/GoogleResource.java
@@ -3,6 +3,7 @@ package app.social.spring.API;
 import app.social.spring.DTO.TokenDTO;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,7 +21,7 @@ public class GoogleResource {
 
     public ResponseEntity<?> loginWithGoogle(@RequestBody TokenDTO tokenDTO) {
         NetHttpTransport transport = new NetHttpTransport();
-        JacksonFactory factory = new
-        GoogleIdTokenVerifier.Builder ver = new GoogleIdTokenVerifier.Builder()
+        JacksonFactory factory = new JacksonFactory();
+        GoogleIdTokenVerifier.Builder ver = new GoogleIdTokenVerifier.Builder(transport, factory);
     }
 }
