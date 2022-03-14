89f8d94fef16f4563fb782eddab571d424c3f801
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 17:12:22 +0300

    adding security configuration

--- src/main/java/app/social/spring/Config/SecurityConfig.java
+++ src/main/java/app/social/spring/Config/SecurityConfig.java
@@ -1,8 +1,14 @@
 package app.social.spring.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
 }
