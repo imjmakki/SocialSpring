b84539f8d79d4c912c7d8d301561396716e1bc31
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 16:52:59 +0300

    Entities Adding

--- src/main/java/app/social/spring/Entity/Student.java
+++ src/main/java/app/social/spring/Entity/Student.java
@@ -1,7 +1,14 @@
 package app.social.spring.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Student implements Serializable {
 
     private Long id;
