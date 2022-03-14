39e8e2d819e914f3b91e0c9b4304b5802c2db44c
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-14 18:22:24 +0300

    adding the Repositories

--- src/main/java/app/social/spring/Service/RoleService.java
+++ src/main/java/app/social/spring/Service/RoleService.java
@@ -1,7 +1,16 @@
 package app.social.spring.Service;
 
+import app.social.spring.DAO.RoleDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService {
+
+    private RoleDAO roleDAO;
+
+    @Autowired
+    public RoleService(RoleDAO roleDAO) {
+        this.roleDAO = roleDAO;
+    }
 }
