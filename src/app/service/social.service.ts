82748a4e88583946965ee2bf16689f5a6715cf9b
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 18:28:23 +0300

    Sign out from everything

--- src/app/service/social.service.ts
+++ src/app/service/social.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
@@ -7,9 +7,26 @@ import {HttpClient} from "@angular/common/http";
 })
 export class SocialService {
 
+  private baseUrl = 'http://localhost:8080/social/';
   constructor(private http: HttpClient) { }
 
-  loginWithGoogle(token): Observable<any>{
+  loginWithGoogle(token: any): Observable<any>{
+    return this.http.post(`${this.baseUrl}google`, {token}).pipe(
+      map(
+        response => {
+          return response;
+        }
+      )
+    );
+  }
 
+  loginWithFacebook(token: any): Observable<any>{
+    return this.http.post(`${this.baseUrl}facebook`, {token}).pipe(
+      map(
+        response => {
+          return response;
+        }
+      )
+    );
   }
 }
