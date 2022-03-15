546ab3984349ec5b9063d47b0e1468fdd95a5eb3
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 19:49:27 +0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   SocialLoginModule
 } from "angularx-social-login";
 import {HttpClientModule} from "@angular/common/http";
+import { StudentComponent } from './layout/student/student.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -29,7 +30,8 @@ const routes: Routes = [
     HeaderComponent,
     SocialComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    StudentComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
