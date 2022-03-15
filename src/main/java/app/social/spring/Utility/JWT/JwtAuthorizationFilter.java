f2a67d6b13816ab51d13f7088b3fb30de67606d0
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 14:01:11 +0300

    Adding Data Transfer Object

---
+++ src/main/java/app/social/spring/Utility/JWT/JwtAuthorizationFilter.java
@@ -0,0 +1,62 @@
+package app.social.spring.Utility.JWT;
+
+import app.social.spring.DAO.UserDAO;
+import app.social.spring.Entity.User;
+import app.social.spring.Utility.Config.UserPrincipal;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private UserDAO userDAO;
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserDAO userDAO) {
+        super(authenticationManager);
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(JwtProperties.HEADER_STRING);
+        if (header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        Authentication authentication = getUsernamePasswordAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        chain.doFilter(request, response);
+    }
+
+    private Authentication getUsernamePasswordAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(JwtProperties.HEADER_STRING)
+                .replace(JwtProperties.TOKEN_PREFIX, "");
+
+        if (token != null) {
+            String email = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()))
+                    .build()
+                    .verify(token)
+                    .getSubject();
+            if (email != null) {
+                User user = userDAO.findByEmail(email);
+                UserPrincipal principal = new UserPrincipal(user);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(email, null, principal.getAuthorities());
+                return auth;
+            }
+            return null;
+        }
+        return null;
+    }
+}
