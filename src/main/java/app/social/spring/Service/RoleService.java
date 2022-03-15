84e5d8a7e59c5e0fb2b52219e481f5fec749399c
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 16:02:58 +0300

    connecting back end with the front

--- src/main/java/app/social/spring/Service/RoleService.java
+++ src/main/java/app/social/spring/Service/RoleService.java
@@ -1,8 +1,11 @@
 package app.social.spring.Service;
 
 import app.social.spring.DAO.RoleDAO;
+import app.social.spring.Entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoleService {
@@ -12,5 +15,9 @@ public class RoleService {
     @Autowired
     public RoleService(RoleDAO roleDAO) {
         this.roleDAO = roleDAO;
+    }
+
+    public List<Role> getRoles() {
+        return roleDAO.findAll();
     }
 }
