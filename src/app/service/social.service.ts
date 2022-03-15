01b1d9028e16e4a4b1a51096b844275713ebab48
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 18:05:27 +0300

    Sign out from everything

--- src/app/service/social.service.ts
+++ src/app/service/social.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocialService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loginWithGoogle(token): Observable<any>{
+
+  }
 }
