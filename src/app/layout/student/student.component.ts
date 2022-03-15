8bbd891cb7e5c62d0a54a0613c1cd29eb2701c39
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 20:09:57 +0300

    components

--- src/app/layout/student/student.component.ts
+++ src/app/layout/student/student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StudentService} from "../../service/student.service";
 
 @Component({
   selector: 'app-student',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private student: StudentService) { }
 
   ngOnInit(): void {
   }
