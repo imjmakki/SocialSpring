5681c175f39ad91f1472f52d5d12c0d1f1b037cc
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 12:00:13 +0300

    user service and security configuration

--- src/main/java/app/social/spring/Utility/DBTest/Runner.java
+++ src/main/java/app/social/spring/Utility/DBTest/Runner.java
@@ -4,14 +4,17 @@ import app.social.spring.DAO.UserDAO;
 import app.social.spring.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class Runner implements CommandLineRunner {
 
     private UserDAO userDAO;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public Runner(UserDAO userDAO) {
+    public Runner(UserDAO userDAO, PasswordEncoder passwordEncoder) {
         this.userDAO = userDAO;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
