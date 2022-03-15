216d9d2709b3090ba2d5144d79ffedb4feaf3942
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-15 14:30:39 +0300

    Adding Data Transfer Object

--- src/main/java/app/social/spring/API/AccessResource.java
+++ src/main/java/app/social/spring/API/AccessResource.java
@@ -5,6 +5,7 @@ import app.social.spring.DTO.LoginResponse;
 import app.social.spring.Utility.Implementation.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class AccessResource {
     }
 
     @PostMapping("/login")
-    public LoginResponse login(JwtLogin jwtLogin) throws Exception {
+    public LoginResponse login(@RequestBody JwtLogin jwtLogin) throws Exception {
         return tokenService.login(jwtLogin);
     }
 }
